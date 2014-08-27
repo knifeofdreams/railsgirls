c2c517a3fc9e3e68936dff2a8e04e3fb5edeec91
knifeofdreams <knifeofdreams86@gmail.com>
2014-08-27 10:45:58 +0200

    Blank rails project created, idea scaffold has been generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '0e5f6af6f475c7322dd57ce760dc57f13ca1b093bb3d3a8a06c25bdacb9e8fda65e9a1d1b39c0c899b76657ec6581bfbdf47e98de9dc651bfe79f91718fdf3d0'
