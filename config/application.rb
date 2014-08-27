de271c49bdebdd37de4fe40290869afd46b3a93a
knifeofdreams <knifeofdreams86@gmail.com>
2014-08-27 20:09:25 +0200

    PictureUploader enabled again

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'carrierwave'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -11,8 +12,6 @@ end
 
 module Railsgirls
   class Application < Rails::Application
-
-    config.autoload_paths += "#{Rails.root}/app/uploaders"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
